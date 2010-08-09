b125e46e80cf68c75ec043dda8635d4bda45317a
Andreas Stuehrk <andy@hammerhartes.de>
2010-08-09 22:37:36 +0200

    Make html-only posts editable.

--- schrift.py
+++ schrift.py
@@ -440,17 +440,21 @@ def save_entry():
         flask.abort(404)
     if entry.author.id != flask.session["user_id"]:
         flask.abort(403)
-    entry.content = form["content"]
+    if not entry.content and entry.html:
+        entry.html = form["content"]
+    else:
+        entry.content = form["content"]
     entry.tags = get_tags(form["tags"])
     entry.private = "private" in form
     if not form["title"]:
         flask.flash("Sorry, but a title is required.")
         return flask.render_template("edit.html", entry=entry)
-    summary_parts = docutils.core.publish_parts(form["summary"], 
-                                                writer=Writer())
-    parts = docutils.core.publish_parts(form["content"], writer=Writer())
-    entry.summary_html = summary_parts["body"]
-    entry.html = parts["body"]
+    if entry.content or not entry.html:
+        summary_parts = docutils.core.publish_parts(form["summary"], 
+                                                    writer=Writer())
+        parts = docutils.core.publish_parts(form["content"], writer=Writer())
+        entry.summary_html = summary_parts["body"]
+        entry.html = parts["body"]
     db.session.commit()
     flask.flash('Post "%s" has been updated.' % (entry.title, ))
     return flask.redirect(flask.url_for("show_entry", slug=entry.slug))
